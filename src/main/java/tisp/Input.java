8e11e5a642782d6419d48d1836c847481a94de5d
Spiffyk <oto.stava@gmail.com>
2017-11-19 19:46:16 +0100

    Use a transition map, add javadoc

--- src/main/java/tisp/Input.java
+++ src/main/java/tisp/Input.java
@@ -1,9 +1,33 @@
 package tisp;
 
+/**
+ * The input signals for the garage door control automaton.
+ */
 public enum Input {
+
+	/**
+	 * The control button signal. Triggered when a user pushes the button.
+	 */
 	BUTTON,
+
+	/**
+	 * The light timeout timer signal. Triggered when the timer runs out.
+	 */
 	TIMER,
+
+	/**
+	 * The upper trigger signal. Triggered when the door is fully open.
+	 */
 	TRIGGER_UPPER,
+
+	/**
+	 * The lower trigger signal. Triggered when the door is fully closed.
+	 */
 	TRIGGER_LOWER,
+
+	/**
+	 * The photocell signal. Triggered by the photocell (e.g. when a car drives through while the door is closing).
+	 */
 	PHOTOCELL
+
 }
--- src/main/java/tisp/State.java
+++ src/main/java/tisp/State.java
@@ -1,8 +1,14 @@
 package tisp;
 
-import lombok.Getter;
+import lombok.NonNull;
 import lombok.ToString;
 
+import java.util.HashMap;
+
+/**
+ * The state enum of the garage door control automaton. Each enum has a table of transitions for {@link Input}s defined.
+ * There is a light in the garage: unless the name of the state explicitly specifies it, the light is on.
+ */
 @ToString
 public enum State {
 
@@ -13,49 +19,46 @@ public enum State {
 	CLOSING,
 	STOPPED_WHILE_CLOSING,
 	CLOSED_LIGHT_ON,
-	CLOSED_LIGHT_OFF;
+	CLOSED_LIGHT_OFF,
+	ERROR;
 
 	static {
-		OPEN_LIGHT_ON.transitions = new Transition[] {
-				Transition.get(Input.BUTTON, State.CLOSING),
-				Transition.get(Input.TIMER, State.OPEN_LIGHT_OFF)
-		};
+		OPEN_LIGHT_ON.transitionMap.put(Input.BUTTON, State.CLOSING);
+		OPEN_LIGHT_ON.transitionMap.put(Input.TIMER, State.OPEN_LIGHT_OFF);
 
-		OPEN_LIGHT_OFF.transitions = new Transition[] {
-				Transition.get(Input.BUTTON, State.CLOSING)
-		};
+		OPEN_LIGHT_OFF.transitionMap.put(Input.BUTTON, State.CLOSING);
 
-		OPENING.transitions = new Transition[] {
-				Transition.get(Input.BUTTON, State.STOPPED_WHILE_OPENING),
-				Transition.get(Input.TRIGGER_UPPER, State.OPEN_LIGHT_ON)
-		};
+		OPENING.transitionMap.put(Input.BUTTON, State.STOPPED_WHILE_OPENING);
+		OPENING.transitionMap.put(Input.TRIGGER_UPPER, State.OPEN_LIGHT_ON);
 
-		STOPPED_WHILE_OPENING.transitions = new Transition[] {
-				Transition.get(Input.BUTTON, State.CLOSING)
-		};
+		STOPPED_WHILE_OPENING.transitionMap.put(Input.BUTTON, State.CLOSING);
 
-		CLOSING.transitions = new Transition[] {
-				Transition.get(Input.BUTTON, State.STOPPED_WHILE_CLOSING),
-				Transition.get(Input.TRIGGER_LOWER, State.CLOSED_LIGHT_ON),
-				Transition.get(Input.PHOTOCELL, State.OPENING)
-		};
+		CLOSING.transitionMap.put(Input.BUTTON, State.STOPPED_WHILE_CLOSING);
+		CLOSING.transitionMap.put(Input.TRIGGER_LOWER, State.CLOSED_LIGHT_ON);
+		CLOSING.transitionMap.put(Input.PHOTOCELL, State.OPENING);
 
-		STOPPED_WHILE_CLOSING.transitions = new Transition[] {
-				Transition.get(Input.BUTTON, State.OPENING)
-		};
+		STOPPED_WHILE_CLOSING.transitionMap.put(Input.BUTTON, State.OPENING);
 
-		CLOSED_LIGHT_ON.transitions = new Transition[] {
-				Transition.get(Input.BUTTON, State.OPENING),
-				Transition.get(Input.TIMER, State.CLOSED_LIGHT_OFF)
-		};
+		CLOSED_LIGHT_ON.transitionMap.put(Input.BUTTON, State.OPENING);
+		CLOSED_LIGHT_ON.transitionMap.put(Input.TIMER, State.CLOSED_LIGHT_OFF);
 
-		CLOSED_LIGHT_OFF.transitions = new Transition[] {
-				Transition.get(Input.BUTTON, State.OPENING)
-		};
+		CLOSED_LIGHT_OFF.transitionMap.put(Input.BUTTON, State.OPENING);
 	}
 
-	// TODO - maybe use a map rather than an array of transitions?
+	/**
+	 * A map of transitions for the state.
+	 */
+	private HashMap<Input, State> transitionMap;
 
-	@Getter private Transition[] transitions;
+	/**
+	 * Gets the next state for the specified {@link Input}. If no following state is defined for the {@code Input},
+	 * the {@link State#ERROR} state is returned.
+	 *
+	 * @param input the input for the state
+	 * @return The defined state or {@link State#ERROR} if undefined.
+	 */
+	public State getNextByInput(@NonNull final Input input) {
+		return transitionMap.computeIfAbsent(input, i -> State.ERROR);
+	}
 
 }
